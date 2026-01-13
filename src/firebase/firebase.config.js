c6478e43fd7863f792503eedca730c4d31bd934b
sndgfn <onikuzzaman113@gmail.com>
2026-01-14 04:23:43 +0600

    first commit

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -3,15 +3,6 @@ import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-const firebaseConfig = {
-  apiKey: "AIzaSyARaQmFdsVOjcxkKkdzTxWD-1MHMkBNWpY",
-  authDomain: "bangla-inovator-client.firebaseapp.com",
-  projectId: "bangla-inovator-client",
-  storageBucket: "bangla-inovator-client.firebasestorage.app",
-  messagingSenderId: "740918149972",
-  appId: "1:740918149972:web:3b8c6bfa75c11456a6bec1",
-  measurementId: "G-N0FE2ZTF7D"
-};
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
